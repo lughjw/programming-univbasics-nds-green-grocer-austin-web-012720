e26bfb0a7be59ad6e4904da9e1dbb96aabf836d6
Lugh Wilson <lughjw@gmail.com>
2020-01-02 20:28:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -77,15 +77,16 @@ def apply_coupon(cart, coupon)
   # GOAL: Apply the coupon to the cart
   # RETURN: the cart with the applied coupon
   item = find_item_by_name_in_collection(coupon[:item], cart)
-  if(item[:count] == coupon[:count])
+  if(item[:count] == coupon[:num])
     item[:item] += " W/ COUPON"
-    item[:price] = coupon[:price]/coupon[:count]
+    item[:price] = coupon[:cost]/coupon[:num]
   else
     item_with_coupon = consolidate_cart_item(item)
     item_with_coupon[:item] += " W/ COUPON"
-    item_with_coupon[:price] = coupon[:price]/coupon[:count]
+    item_with_coupon[:price] = coupon[:cost]/coupon[:num]
+    item[:count] -= coupon[:num]
   end
-  
+  cart
 end
 
 def apply_clearance(cart)
