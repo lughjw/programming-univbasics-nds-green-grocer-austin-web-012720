0b1eb0ec099b5f50ebf22c8a7576b555249db1c3
Lugh Wilson <lughjw@gmail.com>
2020-01-02 20:57:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -116,7 +116,8 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
   
-  consolidate_cart(cart)
+  con_cart = consolidate_cart(cart)
+  
   apply_coupons(cart, coupons)
   apply_clearance(cart)
   
